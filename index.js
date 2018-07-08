3d5e9c1949590078d2278b1d84b53e28f22b4b5f
Vivek Sinha <viveksinha461@gmail.com>
2018-07-08 15:24:37 +0530

    css file copy enable flag a added

--- index.js
+++ index.js
@@ -3,8 +3,24 @@ var crypto = require('crypto');
 var fs = require('fs');
 var mkdirp = require('mkdirp');
 var Path = require('path');
+var rimraf = require('rimraf');
+
 module.exports = {
-    fileNameGeneration : function(path, cssPath, length){
+
+    /**
+     * @author Vivek Sinha
+     * @augments options{path, cssPath, length, cssEnabled}
+     * @param path path of configuration which will have css and js filename
+     * @param cssPath path of css path
+     * @param length length of filename
+     * @param cssEnabled if true css file random generation will happen
+     * else filename will be the same
+     */
+    fileNameGeneration : function(options){
+        let path = options.path;
+        let cssPath = options.cssPath;
+        let length = options.length;
+        let cssEnabled = options.cssEnabled;
             if(!path){
                 path = 'buildDetails.json';
             }
@@ -22,9 +38,18 @@ module.exports = {
                 length = 5;
             }
             let hash = crypto.randomBytes(length).toString('hex');
-            let cssHash = crypto.randomBytes(length).toString('hex');
-            let cssContent = fs.readFileSync(cssPath);
-            fs.writeFileSync(`${Path.dirname(cssPath)}/${cssHash}.css`, cssContent)
+            let cssHash = '';
+            if(cssEnabled){
+                let cssHash = crypto.randomBytes(length).toString('hex');
+                let cssContent = fs.readFileSync(cssPath);
+                rimraf.sync(`${Path.dirname(cssPath)}/copiedStyles`);
+                mkdirp.sync(`${Path.dirname(cssPath)}/copiedStyles`);
+                fs.writeFileSync(`${Path.dirname(cssPath)}/copiedStyles/${cssHash}.css`, cssContent)
+            }
+            else{
+                let cssFileParts = cssPath.split('/');
+                cssHash = cssFileParts[cssFileParts.length - 1].replace('.css', '');
+            }
             let data = {
                 jsFileName:hash,
                 cssFileName:cssHash
